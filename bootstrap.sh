762876e33110ab8fb63621e696803ab9087732b7
Jeffrey Olsen <jeffrey.olsen@dell.com>
2017-05-19 16:36:02 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,6 @@
 apt-get update
 apt-get upgrade -y
 
-#Install Chef Solo
-curl -L https://www.opscode.com/chef/install.sh | bash
-
 #Download Cloud Foundry CLI
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
@@ -17,9 +14,6 @@ sudo apt-get install cf-cli
 
 #Install Docker Compose
 apt install docker-compose -y
-
-#Setup Chef Repository
-curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -c current -P chefdk
 
 git clone https://github.com/thells176/Install-concourseci.git
 cd Install-concourseci
