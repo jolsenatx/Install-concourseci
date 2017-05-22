b98ab53a061aeebd94687a60889465b954482712
jolsenatx <jeffrey.olsen@dell.com>
2017-05-22 09:38:10 -0500

    Added Concourse FLY CLI install

--- jenkins/bootstrap_jumpbox.sh
+++ jenkins/bootstrap_jumpbox.sh
@@ -10,4 +10,9 @@ echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/a
 sudo apt-get update
 
 #Install Cloud Foundry CLI
-sudo apt-get install cf-cli -y
+sudo apt-get install cf-cli
+
+#Install Concourse Fly CLI on jumpbox VM
+curl -L https://github.com/concourse/fly/releases/download/v2.5.1-rc.9/fly_linux_amd64 -o fly
+chmod +x fly
+sudo mv fly /usr/bin/fly 
