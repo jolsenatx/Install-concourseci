bde824836ba13413ba3eb025743e0f1b83dbf07b
jolsenatx <jeffrey.olsen@dell.com>
2017-05-22 09:36:41 -0500

    removed Concourse Fly CLI from setup

--- jenkins/bootstrap_jumpbox.sh
+++ jenkins/bootstrap_jumpbox.sh
@@ -10,9 +10,4 @@ echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/a
 sudo apt-get update
 
 #Install Cloud Foundry CLI
-sudo apt-get install cf-cli
-
-#Install Concourse Fly CLI on jumpbox VM
-curl -L https://github.com/concourse/fly/releases/download/v2.5.1-rc.9/fly_linux_amd64 -o fly
-chmod +x fly
-sudo mv fly /usr/bin/fly 
+sudo apt-get install cf-cli -y
