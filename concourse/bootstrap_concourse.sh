40d79661b605e29db3a1aaa2f115b0bbebe6e526
jolsenatx <jeffrey.olsen@dell.com>
2017-05-25 13:04:30 -0500

    Added Artifactory OSS to setup

--- concourse/bootstrap_concourse.sh
+++ concourse/bootstrap_concourse.sh
@@ -6,5 +6,5 @@ apt install docker-compose -y
 #Clone Steven Thellen's (thells176) repository inside the Concourse CI VM and run the install script
 git clone https://github.com/thells176/Install-concourseci.git
 cd Install-concourseci
-./Do_install.sh 192.168.99.101
+./Do_install.sh 192.168.99.102
 
